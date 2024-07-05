e080a52dfe5041da6bbc55109f92e91c1bf06163
SanadKhan <khansanad72485@gmail.com>
2024-07-05 22:39:34 +0530

    workspace setup 507

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers'
 import db from '@/lib/supabase/db'
 import { redirect } from 'next/navigation'
 import DashboardSetup from '@/components/dashboard-setup/dashboard-setup'
+import { getUserSubscriptionStatus } from '@/lib/supabase/queries'
 
 const DashboardPage = async() => {
 
@@ -18,9 +19,14 @@ const DashboardPage = async() => {
     where: (workspace, {eq}) => eq(workspace.workspaceOwner, user.id),
   })
 
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  if (subscriptionError) return;
+
   if(!workspace) return (
     <div className='bg-background h-screen w-screen flex justify-center items-center'>
-      {/* <DashboardSetup /> */}
+      <DashboardSetup user={user} subscription={subscription}/>
     </div>
   )
 
